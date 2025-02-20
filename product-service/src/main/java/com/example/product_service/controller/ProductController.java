3ab6bd555a9c23044a286df29c293bf19bcc087f
trongnd106 <trong.nd106@gmail.com>
2025-02-20 22:05:37 +0700

    Init first service

---
+++ product-service/src/main/java/com/example/product_service/controller/ProductController.java
@@ -0,0 +1,35 @@
+package com.example.product_service.controller;
+
+import com.example.product_service.dto.ProductRequest;
+import com.example.product_service.dto.ProductResponse;
+import com.example.product_service.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest){
+        productService.create(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAll();
+    }
+}
